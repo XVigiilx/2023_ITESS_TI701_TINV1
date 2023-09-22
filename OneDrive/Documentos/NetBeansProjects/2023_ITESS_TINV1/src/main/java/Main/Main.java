6260db81dbed197f507c93a657431223fcef2506
XVigiilx <luisdejesusvigilbarrios@gmail.com>
2023-09-22 15:29:20 -0600

    First commit

---
+++ OneDrive/Documentos/NetBeansProjects/2023_ITESS_TINV1/src/main/java/Main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * ITESS TICS
+ * TALLER DE INVESTIGACION
+ * INTENTO DE CURSO DE CERTIFICACION DE JAVA 0Z1-808
+ * BY LDJVB
+ *22 DE SEPTIEMBRE DEL 2023
+ */
+package Main;
+
+/**
+ *
+ * @author luisd
+ */
+public class Main {
+    public static void main (String args[]){
+        
+    }
+    
+}
